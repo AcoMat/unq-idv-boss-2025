81e77db923062e12e98b2a7e33a77e5b750c8088
ivanros02 <ivanrosendo1102@gmail.com>
2025-05-07 09:13:35 -0300

    jump

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+var is_charging_jump := false
+var jump_charge := 0.0
+
+@export var max_jump_force := 600.0
+@export var charge_rate := 500.0
 @export var speed: float = 200.0
 @export var jump_force: float = 250.0
 @export var gravity: float = 200.0
@@ -12,8 +17,20 @@ func _physics_process(delta: float) -> void:
 	if (!is_on_floor()):
 		velocity.y += gravity * delta
 		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		jump()
+# Inicia carga si se presiona salto estando en el suelo
+	if Input.is_action_pressed("jump") and is_on_floor():
+		is_charging_jump = true
+		jump_charge = min(jump_charge + charge_rate * delta, max_jump_force)
+
+# Al soltar salto, ejecuta el salto con la fuerza cargada
+	if Input.is_action_just_released("jump") and is_charging_jump:
+		if is_on_floor():
+			velocity.y = -jump_charge
+			is_control_enabled = false
+			$FallCooldown.start()
+		is_charging_jump = false
+		jump_charge = 0.0
+
 		
 	if is_control_enabled and is_on_floor():
 		var input_vector := Vector2.ZERO
@@ -43,15 +60,6 @@ func apply_knockback(intertia: Vector2):
 	var knockback_dir: float = -sign(intertia.x)
 	velocity.x = knockback_dir * knockback_force
 	$KnockDownCooldown.start()
-
-
-func jump():
-	if(is_on_floor()):
-		is_control_enabled = false
-		print(is_control_enabled)
-		print("JUMP")
-		velocity.y -= jump_force
-
 
 func _on_fall_cooldown_timeout() -> void:
 	is_control_enabled = true
